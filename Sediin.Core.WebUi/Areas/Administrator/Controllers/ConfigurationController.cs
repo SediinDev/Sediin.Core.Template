836d7c3806ade7629a5aba248b04c2b74e289142
c.galletti@sediin.it <c.galletti@sediin.it>
2025-08-11 12:47:48 +0200

    email di test

--- Sediin.Core.WebUi/Areas/Administrator/Controllers/ConfigurationController.cs
+++ Sediin.Core.WebUi/Areas/Administrator/Controllers/ConfigurationController.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HtmlAgilityPack;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
 using Sediin.Core.TemplateConfiguration.Models;
+using Sediin.Core.WebUi.Areas.Administrator.Models;
 using Sediin.Core.WebUi.Filters;
+using System.Configuration;
 using System.Threading.Tasks;
 
 namespace Sediin.Core.WebUi.Areas.Administrator.Controllers
@@ -32,6 +36,36 @@ namespace Sediin.Core.WebUi.Areas.Administrator.Controllers
         {
             await _sediinConfiguration.SaveEmailSettings(model);
             return Content("Email settings aggiornati");
+        }
+
+        public async Task<IActionResult> EmailTest()
+        {
+            var model = new TestMailSettingConfigModel();
+            return AjaxView(model: model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EmailTest(TestMailSettingConfigModel model)
+        {
+            if(model == null)
+                return Content("Email Test non valorizzata!");
+            if (model.EmailTo == null)
+                return Content("Email Test non valorizzata!");
+            if (model.Oggetto == null)
+                model.Oggetto = "";
+            if (model.Messaggio == null)
+                model.Messaggio = "";
+
+            var message = new SimpleMailMessage
+            {
+                Body = model.Messaggio,
+                Subject = model.Oggetto,
+                ToEmail = model.EmailTo
+            };
+
+            await _emailSender.SendEmailAsync(message.ToEmail, message.Subject, message.Body);
+
+            return Content("Email Test inviata a " + model.EmailTo);
         }
     }
 }
