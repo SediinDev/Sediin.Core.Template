e3de9be27800dec21637244a13d4ba70ae8af32d
Pino Tuzzolino <g.tuzzolino@sediin.it>
2025-08-13 10:59:24 +0200

    -

--- Sediin.Core.WebUi/Areas/TemplateEmail/Models/AuthenticationModel.cs
+++ Sediin.Core.WebUi/Areas/TemplateEmail/Models/AuthenticationModel.cs
@@ -2,19 +2,19 @@
 {
     public class ConfirmEmailModel
     {
-        public string Url { get; set; }
-        public string Nome { get; set; }
-        public string Cognome { get; set; }
-        public string Email { get; set; }
-        public string Username { get; set; }
+        public string? Url { get; set; }
+        public string? Nome { get; set; }
+        public string? Cognome { get; set; }
+        public string? Email { get; set; }
+        public string? Username { get; set; }
     }
 
     public class ResetPasswordModel
     {
-        public string Url { get; set; }
-        public string Nome { get; set; }
-        public string Cognome { get; set; }
-        public string Email { get; set; }
-        public string Username { get; set; }
+        public string? Url { get; set; }
+        public string? Nome { get; set; }
+        public string? Cognome { get; set; }
+        public string? Email { get; set; }
+        public string? Username { get; set; }
     }
 }
